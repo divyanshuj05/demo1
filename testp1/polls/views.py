f6188c5fea0ee7e656489538bc4563f218ece0c2
divyanshuj05 <djain60_be20@thapar.edu>
2021-03-29 17:44:06 +0530

    29 march

--- testp1/polls/views.py
+++ testp1/polls/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the Django , this is our very first polls webapp")
-
-
+def detail(request, question_id):
+    return HttpResponse("this is the detail view of the question : %s" % question_id)
+def results(request, question_id):
+    return HttpResponse("these are the results of the question : %s" % question_id)
+def vote(request, question_id):
+    return HttpResponse("Vote on  question : %s" % question_id)
