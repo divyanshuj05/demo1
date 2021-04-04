e272440eb2431916e97f78b73f05925ec80d3337
divyanshuj05 <djain60_be20@thapar.edu>
2021-04-05 00:32:36 +0530

    29 march

--- testp1/polls/views.py
+++ testp1/polls/views.py
@@ -1,12 +1,32 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Welcome to the Django , this is our very first polls webapp")
+    latest_questions = Question.objects.order_by('pub_date')[:5]
+    context = {'latest_questions':latest_questions}
+    return render(request, 'polls/index.html', context)
+
 def detail(request, question_id):
-    return HttpResponse("this is the detail view of the question : %s" % question_id)
+    question= get_object_or_404(Question,pk = question_id)
+    return render(request,'polls/detail.html', {'question':question})
+
 def results(request, question_id):
-    return HttpResponse("these are the results of the question : %s" % question_id)
+   question = get_object_or_404(Question,pk = question_id)
+   return render(request,'polls/results.html',{'question':question})
+
+
 def vote(request, question_id):
-    return HttpResponse("Vote on  question : %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {'question':question, 'error_message': "Plz select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results',args = (question.id,)))
--- testp1/testp1/urls.py
+++ testp1/testp1/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('/polls', include('polls.urls')),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include(('polls.urls','polls'), namespace="polls")),
+
 ]
